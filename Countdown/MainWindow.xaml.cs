b14f1764ba1f439145b82f508930b67eadd2fbd5
Herohtar <belac1186@gmail.com>
2017-01-07 01:55:04 -0600

    Initial commit.

---
+++ Countdown/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Countdown
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string timeLeft;
+        private DateTime target = new DateTime(2017, 4, 8, 14, 4, 0);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            PeriodicTask.Run(updateCountdown, TimeSpan.FromMilliseconds(1));
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var hwnd = new WindowInteropHelper(this).Handle;
+            WindowsServices.SetWindowExTransparent(hwnd);
+        }
+
+        public string TimeLeft
+        {
+            get { return this.timeLeft; }
+            set
+            {
+                if (this.timeLeft != value)
+                {
+                    this.timeLeft = value;
+                    RaisePropertyChangedEvent("TimeLeft");
+                }
+            }
+        }
+
+        private void updateCountdown()
+        {
+            TimeSpan difference = target - DateTime.Now;
+            this.TimeLeft = String.Format("{0} days, {1:D2} hours, {2:D2} minutes, {3:D2} seconds, {4:D3} milliseconds", difference.Days, difference.Hours, difference.Minutes, difference.Seconds, difference.Milliseconds);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
