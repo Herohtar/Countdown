1ea3cc88e9b316cacf74934c1e05aef1dd50b8b6
Herohtar <belac1186@gmail.com>
2017-02-04 23:46:35 -0600

    Using Mutex to prevent multiple instances

--- Countdown/App.xaml.cs
+++ Countdown/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,25 @@ namespace Countdown
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        [STAThread]
+        public static void Main()
+        {
+            using (Mutex mutex = new Mutex(false, "8D062F74-D870-47C3-BB61-73C516E58919"))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    return;
+                }
+
+                App app = new App();
+                MainWindow window = new MainWindow();
+                app.Run(window);
+            }
+        }
     }
 }
