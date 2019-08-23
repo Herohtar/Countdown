94907963bbef64ec8a4580dcd6a10b3e7b39c520
Herohtar <belac1186@gmail.com>
2019-08-22 19:16:32 -0500

    Move Main from App to EntryPoint

--- Countdown/EntryPoint.cs
+++ Countdown/EntryPoint.cs
@@ -1,6 +1,24 @@
+using System;
+using System.Threading;
+
 namespace Countdown
 {
   public class EntryPoint
   {
+    [STAThread]
+    public static void Main()
+    {
+      using (Mutex mutex = new Mutex(false, "8D062F74-D870-47C3-BB61-73C516E58919"))
+      {
+        if (!mutex.WaitOne(0, false))
+        {
+          return;
+        }
+
+        App app = new App();
+        MainWindow window = new MainWindow();
+        app.Run(window);
+      }
+    }
   }
 }
